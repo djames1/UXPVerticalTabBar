4ae056ac6c9172fc1b28749518007c999c296b65
Daniel James <djames@orcadian.net>
2020-08-11 14:55:41 -0400

    Initial Commit

---
+++ defaults/preferences/defaults.js
@@ -0,0 +1,7 @@
+pref("extensions.uxp-vtabbar@djames1.description", "chrome://verttabbar/locale/verttabbar.properties");
+pref("verttabbar.width", 200);
+pref("verttabbar.position", "left");
+pref("verttabbar.reverseLabels", false);
+pref("verttabbar.swapButtons", true);
+pref("verttabbar.tabmove", true);
+pref("verttabbar.tabhighlight", false);
